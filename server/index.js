ff3f22811dcaed607713e6178fa969a15e726bb8
Ambreen Zubari <ambreenzubari3ss@gmail.com>
2024-04-28 18:30:05 +0500

    added code

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+// mongoose.connect(process.env.MONGO_URI, {
+// }).then(() => {
+//     console.log("DB Connection Successful");
+// }).catch((err) => {
+//     console.log("ERROR while connecting", err);
+// });
+
+mongoose.connect('mongodb://localhost/notes-db-app',{
+    family: 4,
+}).then(() => {
+        console.log("DB Connection Successful");
+    }).catch((err) => {
+        console.log("ERROR while connecting====================== ", err);
+    });
+    
+
+const server = app.listen(process.env.PORT, () => {
+    console.log('Listening on port ' + process.env.PORT);
+});
